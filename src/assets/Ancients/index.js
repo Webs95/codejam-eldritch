b7eb8cd74409fac9b48092f02161ac3a490e89dd
Webs95 <alexandersemashko1995@yandex.ru>
2022-08-24 11:34:12 +0300

    refactor: change images and imports

--- src/assets/Ancients/index.js
+++ src/assets/Ancients/index.js
@@ -1,7 +1,7 @@
-import azathoth from './Azathoth.png';
-import cthulhu from './Cthulthu.png';
-import iogSothoth from './IogSothoth.png';
-import shubNiggurath from './ShubNiggurath.png';
+import azathoth from './Azathoth.jpg';
+import cthulhu from './Cthulthu.jpg';
+import iogSothoth from './IogSothoth.jpg';
+import shubNiggurath from './ShubNiggurath.jpg';
 
 const ancients = {
   azathoth,
